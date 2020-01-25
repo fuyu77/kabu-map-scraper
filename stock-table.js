4512148d7d66faab69d92dff9bb1983ea1baf0b2
fuyu77 <kiwi.vc@gmail.com>
2020-01-26 01:24:05 +0900

    リファクタ

--- stock-table.js
+++ stock-table.js
@@ -6,16 +6,18 @@ module.exports = class StockTable {
   
   // スクリーニング結果の表の証券コードを取得してthis.companyCodesの配列に追加する
   async setCompanyCodes() {
-    const companyCodes = await this.page.evaluate(() => {
-      const cells = Array.from(document.querySelectorAll('.KM_CODE'))
-      return cells.map((cell) => cell.innerText)
-    })
-    this.companyCodes.push(...companyCodes)
+    const elementHandles = await this.page.$$('.KM_CODE')
+    for (const handle of elementHandles) {
+      const companyCode = await this.page.evaluate((handle) => {
+        return handle.innerText
+      }, handle)
+      this.companyCodes.push(companyCode)
+    }
   }
   
   // ページネーションの次へ進むボタンのElementHandleを取得する
   // 次へボタンが存在しない（ページネーションの終端の）場合、nullを返す
   async setNextButton() {
-    this.nextButton = await this.page.$(".KM_TABLEINDEXNEXT")
+    this.nextButton = await this.page.$('.KM_TABLEINDEXNEXT')
   }
 }
